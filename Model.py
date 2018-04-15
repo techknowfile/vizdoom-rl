2b1293037d860d78f456c8de8d04376f0fde1b12
JoshuaFeinglass <matomot1@aol.com>
2018-04-15 15:58:10 -0700

    Update Model.py

--- Model.py
+++ Model.py
@@ -1,5 +1,5 @@
 from keras.models import Sequential, Model, load_model as lm
-from keras.layers import Dense, Activation, Conv2D, Input, Flatten
+from keras.layers import DropoutDense, Activation, Conv2D, Input, Flatten, MaxPooling2D
 from keras.optimizers import Adam
 import keras
 
@@ -23,10 +23,11 @@ class NeuralModel:
     def model_one(self):
         state_input = Input(shape=(self.kframes, self.resolution[0], self.resolution[1]))
         conv1 = Conv2D(filters=16, kernel_size=3, strides=1, activation='relu', data_format="channels_first")(state_input)
-        flatten = Flatten()(conv1)
+        pool=MaxPooling2D(pool_size=(2, 2), strides=None, padding='valid', data_format=None)(conv1)
+        flatten = Flatten()(pool)
+        dr = Dropout(0.2, input_shape=(128,))(flatten)
         fc1 = Dense(128, activation='relu')(flatten)
-        fc2 = Dense(self.available_actions_count, input_shape=(128,))(fc1)
-
+        fc2 = Dense(self.available_actions_count, input_shape=(128,))(dr)
         model = keras.models.Model(input=state_input, output=fc2)
         adam = Adam(lr=self.learning_rate)
         model.compile(loss="mse", optimizer=adam)
@@ -38,10 +39,14 @@ class NeuralModel:
     def model_two(self):
         state_input = Input(shape=(self.kframes, self.resolution[0], self.resolution[1]))
         conv1 = Conv2D(filters=16, kernel_size=3, strides=1, activation='relu', data_format="channels_first")(state_input)
-        conv2 = Conv2D(filters=32, kernel_size=3, strides=1, activation='relu', data_format="channels_first")(conv1)
-        conv3 = Conv2D(filters=32, kernel_size=2, strides=1, activation='relu', data_format="channels_first")(conv2)
-        flatten = Flatten()(conv3)
-        fc1 = Dense(128, activation='relu')(flatten)
+        pool1=MaxPooling2D(pool_size=(2, 2), strides=None, padding='valid', data_format=None)(conv1)
+        conv2 = Conv2D(filters=32, kernel_size=3, strides=1, activation='relu', data_format="channels_first")(pool1)
+        pool2=MaxPooling2D(pool_size=(2, 2), strides=None, padding='valid', data_format=None)(conv2)
+        conv3 = Conv2D(filters=32, kernel_size=2, strides=1, activation='relu', data_format="channels_first")(pool2)
+        pool3=MaxPooling2D(pool_size=(2, 2), strides=None, padding='valid', data_format=None)(conv3)
+        flatten = Flatten()(pool3)
+        dr = Dropout(0.2, input_shape=(128,))(flatten)
+        fc1 = Dense(128, activation='relu')(dr)
         fc2 = Dense(self.available_actions_count, input_shape=(128,))(fc1)
 
         model = keras.models.Model(input=state_input, output=fc2)
@@ -55,8 +60,10 @@ class NeuralModel:
     def model_three(self):
         state_input = Input(shape=(self.kframes, self.resolution[0], self.resolution[1]))
         conv1 = Conv2D(filters=16, kernel_size=3, strides=1, activation='relu', data_format="channels_first")(state_input)
-        flatten = Flatten()(conv1)
-        fc1 = Dense(256, activation='relu')(flatten)
+        pool1 = Conv2D(filters=32, kernel_size=2, strides=1, activation='relu', data_format="channels_first")(conv1)
+        flatten = Flatten()(pool1)
+        dr = Dropout(0.2, input_shape=(256,))(flatten)
+        fc1 = Dense(256, activation='relu')(dr)
         fc2 = Dense(128, activation='relu')(fc1)
         fc3 = Dense(self.available_actions_count, input_shape=(128,))(fc2)
 
