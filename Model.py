5cb8a0fe59583e7b443345d3e266849b7855e6b5
JoshuaFeinglass <matomot1@aol.com>
2018-04-15 19:16:05 -0700

    Update Model.py

--- Model.py
+++ Model.py
@@ -1,208 +1,75 @@
-from random import sample, randint, random
-import random
-import skimage.color, skimage.transform
-import numpy as np
+from keras.models import Sequential, Model, load_model as lm
+from keras.layers import Dropout, Dense, Activation, Conv2D, Input, Flatten, MaxPooling2D
+from keras.optimizers import Adam
+import keras
 
 
-class ReplayMemory:
-    def __init__(self, capacity, kframes, resolution, test_memory_size):
-        self.resolution = resolution
-        state_shape = (capacity, resolution[0], resolution[1] )
-        test_state_shape = (test_memory_size, resolution[0], resolution[1])
-        self.s1 = np.zeros(state_shape, dtype=np.float32)
-        self.s2 = np.zeros(state_shape, dtype=np.float32)
-        self.a = np.zeros(capacity, dtype=np.int32)
-        self.r = np.zeros(capacity, dtype=np.float32)
-        self.isterminal = np.zeros(capacity, dtype=np.float32)
+class NeuralModel:
+    def __init__(self, available_actions_count, learning_rate, k_frames):
+        self.resolution = [30, 45]
+        self.kframes = k_frames
+        self.available_actions_count = available_actions_count
+        self.learning_rate = learning_rate
 
-        self.test_buffer = np.zeros(test_state_shape, dtype=np.float32)
-        self.test_buff_pos = 0
-        self.test_size = 0
+    def create_model(self, model_type):
+        if model_type == 1:
+            return self.model_one()
+        elif model_type == 2:
+            return self.model_two()
+        elif model_type == 3:
+            return self.model_three()
 
-        self.capacity = capacity
-        self.size = 0
-        self.pos = 0
+    # Conv + dense + dense layer
+    def model_one(self):
+        state_input = Input(shape=(self.kframes, self.resolution[0], self.resolution[1]))
+        conv1 = Conv2D(filters=16, kernel_size=3, strides=1, activation='relu', data_format="channels_first")(state_input)
+        pool=MaxPooling2D(pool_size=(2, 2), strides=None, padding='valid', data_format=None)(conv1)
+        flatten = Flatten()(pool)
+        dr = Dropout(0.2, input_shape=(128,))(flatten)
+        fc1 = Dense(128, activation='relu')(flatten)
+        fc2 = Dense(self.available_actions_count, input_shape=(128,))(dr)
+        model = keras.models.Model(input=state_input, output=fc2)
+        adam = Adam(lr=self.learning_rate)
+        model.compile(loss="mse", optimizer=adam)
+        print(model.summary())
 
-        self.kframes = kframes
+        return state_input, model
 
-    def add_transition(self, s1, action, s2, isterminal, reward):
-        self.s1[self.pos, :, :] = s1
-        self.a[self.pos] = action
-        if not isterminal:
-            self.s2[self.pos, :, :] = s2
-        self.isterminal[self.pos] = isterminal
-        self.r[self.pos] = reward
+    # Conv + Conv + Conv + dense + dense
+    def model_two(self):
+        state_input = Input(shape=(self.kframes, self.resolution[0], self.resolution[1]))
+        conv1 = Conv2D(filters=16, kernel_size=3, strides=1, activation='relu', data_format="channels_first")(state_input)
+        pool1=MaxPooling2D(pool_size=(2, 2), strides=None, padding='valid', data_format=None)(conv1)
+        conv2 = Conv2D(filters=32, kernel_size=3, strides=1, activation='relu', data_format="channels_first")(pool1)
+        pool2=MaxPooling2D(pool_size=(2, 2), strides=None, padding='valid', data_format=None)(conv2)
+        conv3 = Conv2D(filters=32, kernel_size=2, strides=1, activation='relu', data_format="channels_first")(pool2)
+        pool3=MaxPooling2D(pool_size=(2, 2), strides=None, padding='valid', data_format=None)(conv3)
+        flatten = Flatten()(pool3)
+        dr = Dropout(0.2, input_shape=(128,))(flatten)
+        fc1 = Dense(128, activation='relu')(dr)
+        fc2 = Dense(self.available_actions_count, input_shape=(128,))(fc1)
 
-        self.pos = (self.pos + 1) % self.capacity
-        self.size = min(self.size + 1, self.capacity)
+        model = keras.models.Model(input=state_input, output=fc2)
+        adam = Adam(lr=self.learning_rate)
+        model.compile(loss="mse", optimizer=adam)
+        print(model.summary())
 
-    def get_sample(self, sample_size):
-        samples_s1_container = []
-        samples_action_container = []
-        samples_s2_container = []
-        samples_isTerminal_container = []
-        samples_reward_container= []
-        for i in sample(range(0, self.size), sample_size):#sample size is not kframes, but could be 32 or 64 (batch size)
-            frame_indices = range(i-self.kframes+1, i+1)#+1 so that the last data point is considered too.
-            #this will wrap around with negative numbers, so -2, -1,0,1,2 :-)
+        return state_input, model
 
-            #todo, this will be bad training. Need zero padding. mimic the test buffer
+    # Conv + dense + dense + dense + dense layer
+    def model_three(self):
+        state_input = Input(shape=(self.kframes, self.resolution[0], self.resolution[1]))
+        conv1 = Conv2D(filters=16, kernel_size=3, strides=1, activation='relu', data_format="channels_first")(state_input)
+        pool1 = Conv2D(filters=32, kernel_size=2, strides=1, activation='relu', data_format="channels_first")(conv1)
+        flatten = Flatten()(pool1)
+        dr = Dropout(0.2, input_shape=(256,))(flatten)
+        fc1 = Dense(256, activation='relu')(dr)
+        fc2 = Dense(128, activation='relu')(fc1)
+        fc3 = Dense(self.available_actions_count, input_shape=(128,))(fc2)
 
-            s1_data = self.s1[frame_indices]
-            action_data = self.a[i]
-            s2_data = self.s2[frame_indices]
-            isTerminal_data = self.isterminal[i]
-            reward_data = self.r[i]
+        model = keras.models.Model(input=state_input, output=fc3)
+        adam = Adam(lr=self.learning_rate)
+        model.compile(loss="mse", optimizer=adam)
+        print(model.summary())
 
-            #IF we have a terminal frame in the PRECEEDING k-frames then we DO NOT take those frames. Rather we
-            # #repeat the frame after it
-            terminal_offset = 0 #not possible case as you will see
-            if True in self.isterminal[range(i-self.kframes+1, i)]: #dont care if the last one is terminal
-                for k in range(1, self.kframes):#excludes the last index
-                    if self.isterminal[i-k] : terminal_offset = k
-            #--end outer if
-            if terminal_offset != 0: #then we need to repeat some frames
-                #0:-offset  = -offset repeated as many times
-                num_repeats = self.kframes - terminal_offset
-                tmp_reshaped = np.zeros([1] + list(s1_data[-terminal_offset].shape))  # add a leading dummy dimension
-                repeat_frames = np.tile(tmp_reshaped, [num_repeats] + [1] * len(self.resolution))
-                s1_data[0:-terminal_offset] = repeat_frames
-                s2_data[0:-terminal_offset] = repeat_frames
-                #however, the s2 preceeding frame would be s1, so that can be added
-                s2_data[-terminal_offset-1] = s1_data[-terminal_offset]
-
-            samples_s1_container.append(s1_data)
-            samples_action_container.append(action_data)
-            samples_s2_container.append(s2_data)
-            samples_isTerminal_container.append(isTerminal_data)
-            samples_reward_container.append(reward_data)
-
-        return np.array(samples_s1_container),np.array(samples_action_container), \
-               np.array(samples_s2_container),np.array(samples_isTerminal_container),np.array(samples_reward_container)
-
-    def add_to_test_buffer(self, curr_state):
-        self.test_buffer[self.test_buff_pos, :, :] = curr_state
-        self.test_buff_pos = (self.test_buff_pos + 1) % self.capacity
-        self.test_size = min(self.test_size + 1, self.capacity)
-
-    def reset_test_buffer(self):
-        self.test_buffer = np.zeros_like(self.test_buffer, dtype=np.float32)
-        self.test_buff_pos = 0
-        self.test_size = 0
-
-    def get_test_sample(self):
-        #get sample of size kframes, or prepend zeros.
-        return_state_shape = (self.kframes, self.resolution[0], self.resolution[1])
-        ret_buffer = None
-        if self.test_size >= self.kframes:
-            ret_buffer = self.test_buffer[self.test_size-self.kframes:self.test_size,:,:]
-        else:#only fill what we have, and have preceeding zero frames (which was already done)
-            ret_buffer = np.zeros(return_state_shape, dtype=np.float32)
-            ret_buffer[self.kframes-self.test_size:,:,:] = self.test_buffer[:self.test_size, :, :]
-            # num_repeats = kframes-self.test_size
-            # tmp_reshaped = self.test_buffer[0].reshape([1]+list(self.test_buffer[0].shape))#add a leading dummy dimension
-            # repeat_frames = np.tile(tmp_reshaped,[num_repeats]+[1]*len(resolution) )
-            # ret_buffer[:kframes-self.test_size,:,:] = repeat_frames
-        return ret_buffer
-
-
-class DQN:
-    def __init__(self, memory, model, game, actions, resolution, frame_repeat, batch_size, kframes, epochs, discount_factor):
-        self.memory = memory
-        self.model = model
-        self.game = game
-        self.actions = actions
-        self.resolution = resolution
-        self.frame_repeat = frame_repeat
-        self.batch_size = batch_size
-        self.kframes = kframes
-        self.epochs = epochs
-        self.discount_factor = discount_factor
-
-    def preprocess(self, img):
-        img = skimage.transform.resize(img, [self.resolution[0], self.resolution[1]])
-        img = img.astype(np.float32)
-        return img
-
-    def learn_from_memory(self, model):
-        """ Use replay memory to learn. Ignore s2 if s1 is terminal """
-
-        if self.memory.size > self.batch_size:
-            s1, a, s2, isterminal, r = self.memory.get_sample(self.batch_size)
-
-            q = model.predict(s2, batch_size=self.batch_size)
-            q2 = np.max(q, axis=1)
-            target_q = model.predict(s1, batch_size=self.batch_size)
-            target_q[np.arange(target_q.shape[0]), a] = r + self.discount_factor * (1 - isterminal) * q2
-            model.fit(s1, target_q, verbose=0)
-
-    def get_best_action(self, state):
-        q = self.model.predict(state, batch_size=1)
-        m = np.argmax(q, axis=1)[0]
-        action = m  # wrong
-        return action
-
-    def perform_learning_step(self, epoch):
-        """ Makes an action according to eps-greedy policy, observes the result
-        (next state, reward) and learns from the transition"""
-
-        def exploration_rate(epoch, decay = 'log'):
-            """# Define exploration rate change over time"""
-
-            # return 0.1
-            if(decay == 'linear'): 
-                start_eps = 1.0
-                end_eps = 0.1
-                const_eps_epochs = 0.1 * self.epochs  # 10% of learning time
-                eps_decay_epochs = 0.8 * self.epochs  # 60% of learning time
-
-                if epoch < const_eps_epochs:
-                    return start_eps
-                elif epoch < eps_decay_epochs:
-                    # Linear decay
-                    return start_eps - (epoch - const_eps_epochs) / \
-                           (eps_decay_epochs - const_eps_epochs) * (start_eps - end_eps)
-                else:
-                    return end_eps
-            elif(decay == 'log'):
-                steepness=7
-                start_eps = 5.0
-                end_eps = 0.1
-                const_eps_epochs = 0.1 * self.epochs  # 10% of learning time
-                eps_decay_epochs = 0.8 * self.epochs  # 60% of learning time
-
-                if epoch < const_eps_epochs:
-                    return start_eps
-                elif epoch < eps_decay_epochs:
-                    # Logistic decay
-                    return start_eps - np.arctan(steepness*(epoch - const_eps_epochs) / \
-                           (eps_decay_epochs - const_eps_epochs)) / np.arctan(steepness) * (start_eps - end_eps)
-                else:
-                    return end_eps
-        s1 = self.preprocess(self.game.get_state().screen_buffer)
-
-        # With probability eps make a random action.
-        eps = exploration_rate(epoch)
-        if random.random() <= eps:
-            a = randint(0, len(self.actions) - 1)
-        else:
-            # Choose the best action according to the network.
-            self.memory.add_to_test_buffer(s1)
-            state_kframes = self.memory.get_test_sample()
-            state_kframes = state_kframes.reshape([1, self.kframes, self.resolution[0], self.resolution[1]])
-            a = self.get_best_action(state_kframes)
-        reward = self.game.make_action(self.actions[a], self.frame_repeat)
-
-        isterminal = self.game.is_episode_finished()
-        #todo RESET the test buffer ELSEWHERE. not here
-
-        s2 = self.preprocess(self.game.get_state().screen_buffer) if not isterminal else None
-
-        # Remember the transition that was just experienced.
-        self.memory.add_transition(s1, a, s2, isterminal, reward)
-
-        self.learn_from_memory(self.model)
-
-
-
-
+        return state_input, model
