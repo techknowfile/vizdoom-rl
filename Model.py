37022e0e8d39ceabdcd0474d640c323749d96b8c
Unknown <marirsg2@gmail.com>
2018-04-16 16:14:06 -0700

    updated CNN model to match original model
    Only CNN and lstm are left

--- Model.py
+++ Model.py
@@ -15,17 +15,19 @@ class NeuralModel:
         if model_type == 1:
             return self.model_one()
         elif model_type == 2:
-            return self.model_two()
+            return self.model_one()
         elif model_type == 3:
-            return self.model_three()
+            return self.model_one()
         elif model_type == 4:
             return self.model_LSTM_one()
+
 
     # Conv + dense + dense layer
     def model_one(self):
         state_input = Input(shape=(self.kframes, self.resolution[0], self.resolution[1]))
-        conv1 = Conv2D(filters=16, kernel_size=3, strides=1, activation='relu', data_format="channels_first")(state_input)
-        flatten = Flatten()(conv1)
+        conv1 = Conv2D(filters=8, kernel_size=6, strides=1, activation='relu', data_format="channels_first")(state_input)
+        conv2 = Conv2D(filters=8, kernel_size=3, strides=1, activation='relu', data_format="channels_first")(conv1)
+        flatten = Flatten()(conv2)
         fc1 = Dense(128, activation='relu')(flatten)
         fc2 = Dense(self.available_actions_count, input_shape=(128,))(fc1)
 
@@ -36,38 +38,53 @@ class NeuralModel:
 
         return state_input, model
 
-    # Conv + Conv + Conv + dense + dense
-    def model_two(self):
-        state_input = Input(shape=(self.kframes, self.resolution[0], self.resolution[1]))
-        conv1 = Conv2D(filters=16, kernel_size=3, strides=1, activation='relu', data_format="channels_first")(state_input)
-        conv2 = Conv2D(filters=32, kernel_size=3, strides=1, activation='relu', data_format="channels_first")(conv1)
-        conv3 = Conv2D(filters=32, kernel_size=2, strides=1, activation='relu', data_format="channels_first")(conv2)
-        flatten = Flatten()(conv3)
-        fc1 = Dense(128, activation='relu')(flatten)
-        fc2 = Dense(self.available_actions_count, input_shape=(128,))(fc1)
-
-        model = keras.models.Model(input=state_input, output=fc2)
-        adam = Adam(lr=self.learning_rate)
-        model.compile(loss="mse", optimizer=adam)
-        print(model.summary())
-
-        return state_input, model
-
-    # Conv + dense + dense + dense + dense layer
-    def model_three(self):
-        state_input = Input(shape=(self.kframes, self.resolution[0], self.resolution[1]))
-        conv1 = Conv2D(filters=16, kernel_size=3, strides=1, activation='relu', data_format="channels_first")(state_input)
-        flatten = Flatten()(conv1)
-        fc1 = Dense(256, activation='relu')(flatten)
-        fc2 = Dense(128, activation='relu')(fc1)
-        fc3 = Dense(self.available_actions_count, input_shape=(128,))(fc2)
-
-        model = keras.models.Model(input=state_input, output=fc3)
-        adam = Adam(lr=self.learning_rate)
-        model.compile(loss="mse", optimizer=adam)
-        print(model.summary())
-
-        return state_input, model
+    # # Conv + dense + dense layer
+    # def model_one(self):
+    #     state_input = Input(shape=(self.kframes, self.resolution[0], self.resolution[1]))
+    #     conv1 = Conv2D(filters=16, kernel_size=3, strides=1, activation='relu', data_format="channels_first")(state_input)
+    #     flatten = Flatten()(conv1)
+    #     fc1 = Dense(128, activation='relu')(flatten)
+    #     fc2 = Dense(self.available_actions_count, input_shape=(128,))(fc1)
+    #
+    #     model = keras.models.Model(input=state_input, output=fc2)
+    #     adam = Adam(lr=self.learning_rate)
+    #     model.compile(loss="mse", optimizer=adam)
+    #     print(model.summary())
+    #
+    #     return state_input, model
+    #
+    # # Conv + Conv + Conv + dense + dense
+    # def model_two(self):
+    #     state_input = Input(shape=(self.kframes, self.resolution[0], self.resolution[1]))
+    #     conv1 = Conv2D(filters=16, kernel_size=3, strides=1, activation='relu', data_format="channels_first")(state_input)
+    #     conv2 = Conv2D(filters=32, kernel_size=3, strides=1, activation='relu', data_format="channels_first")(conv1)
+    #     conv3 = Conv2D(filters=32, kernel_size=2, strides=1, activation='relu', data_format="channels_first")(conv2)
+    #     flatten = Flatten()(conv3)
+    #     fc1 = Dense(128, activation='relu')(flatten)
+    #     fc2 = Dense(self.available_actions_count, input_shape=(128,))(fc1)
+    #
+    #     model = keras.models.Model(input=state_input, output=fc2)
+    #     adam = Adam(lr=self.learning_rate)
+    #     model.compile(loss="mse", optimizer=adam)
+    #     print(model.summary())
+    #
+    #     return state_input, model
+    #
+    # # Conv + dense + dense + dense + dense layer
+    # def model_three(self):
+    #     state_input = Input(shape=(self.kframes, self.resolution[0], self.resolution[1]))
+    #     conv1 = Conv2D(filters=16, kernel_size=3, strides=1, activation='relu', data_format="channels_first")(state_input)
+    #     flatten = Flatten()(conv1)
+    #     fc1 = Dense(256, activation='relu')(flatten)
+    #     fc2 = Dense(128, activation='relu')(fc1)
+    #     fc3 = Dense(self.available_actions_count, input_shape=(128,))(fc2)
+    #
+    #     model = keras.models.Model(input=state_input, output=fc3)
+    #     adam = Adam(lr=self.learning_rate)
+    #     model.compile(loss="mse", optimizer=adam)
+    #     print(model.summary())
+    #
+    #     return state_input, model
 
     # Conv + dense + dense + dense + dense layer
     def model_LSTM_one(self):
